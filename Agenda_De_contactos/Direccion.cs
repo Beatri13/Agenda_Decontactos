af0d348040aaee19c3944139a2966f0981c6a4ee
beatri1304 <162139180+Beatri13@users.noreply.github.com>
2025-03-25 21:42:45 -0600

    Direccion
    Beatriz Robles

--- Agenda_De_contactos/Direccion.cs
+++ Agenda_De_contactos/Direccion.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 
-public class Class1
-{
-    public Class1()
-
-    {
     public class Direccion
     {
         public string Calle { get; set; }
         public string Numero { get; set; }
         public string Colonia { get; set; }
+        public string Ciudad { get; set; }
         public string Departamento { get; set; }
 
-        public Direccion(string calle, string numero, string colonia, string departamento)
+        
+        public Direccion(string calle, string numero, string colonia, string ciudad, string departamento)
         {
             Calle = calle;
             Numero = numero;
             Colonia = colonia;
+            Ciudad = ciudad;
             Departamento = departamento;
         }
- 
-
     }
-}
+
+
+    
+
 
 	
 
